00ceddeda398d93a4f06a7b90e020ab55b25bfe8
Dalton <Dtot360@gmail.com>
2014-05-14 12:56:07 -0400

    Physics update

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -9,51 +9,28 @@ public class Gravity : MonoBehaviour {
 	public float distancetohitground;
 
 	RaycastHit rFront;
-	RaycastHit rLeft;
-	RaycastHit rRight;
 	RaycastHit rMiddle;
-	RaycastHit rBack;
+
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		#region Racast stuff
-		//Raycasts
-
 
 		//Raycasts actually launching
 		Vector3 TempPosition = transform.position + Vector3.up * 1f;
 		Physics.Raycast (TempPosition + (transform.forward *  3), Vector3.down,out rFront, 500, lWater);
 		Physics.Raycast (TempPosition , Vector3.down,out rMiddle, 500,lWater);
-		Physics.Raycast (TempPosition - (transform.right * 2)+ (transform.forward *  3), Vector3.down,out rLeft, 500, lWater);
-		Physics.Raycast (TempPosition + (transform.right * 2)+ (transform.forward *  3), Vector3.down,out rRight,500, lWater);
-		Physics.Raycast (TempPosition - (transform.forward *  3), Vector3.down,out rBack, 500,lWater);
-		//Debug Drawing the raycasts
-		Debug.DrawRay (TempPosition + (transform.forward *  3), Vector3.down, Color.blue);
-		Debug.DrawRay (TempPosition , Vector3.down, Color.blue);
-		Debug.DrawRay (TempPosition - (transform.forward *  3), Vector3.down, Color.blue);
-		Debug.DrawRay (TempPosition - (transform.right * 2)+ (transform.forward *  3), Vector3.down, Color.green);
-		Debug.DrawRay (TempPosition + (transform.right * 2)+ (transform.forward *  3), Vector3.down, Color.green);
+	
 		#endregion
 
 
 
-		if(rFront.distance > distancetohitground && rBack.distance > distancetohitground && rLeft.distance > distancetohitground && rRight.distance > distancetohitground && rMiddle.distance > distancetohitground)
-		{
-			//timeProj-= Time.deltaTime;
-		
-			Vector3 temp;
-			temp = transform.position;
-			//temp.y += 0.01f;
-			transform.position = temp;
-			
-		}
 		//Checks closeness to the gound, stops falling if close enough
-		if(rFront.distance > distancetohitground && rBack.distance > distancetohitground && rLeft.distance > distancetohitground && rRight.distance > distancetohitground && rMiddle.distance > distancetohitground)
+		if(rFront.distance > distancetohitground)
 		{
 			//timeProj-= Time.deltaTime;
 
@@ -67,30 +44,23 @@ public class Gravity : MonoBehaviour {
 
 			#region Angling the object
 			Vector3 vAngle = transform.eulerAngles;
-			//Angles stuff, I'll probably be fixing this up more later on
-			//it's a bit janky
-			if( rFront.distance < rBack.distance)
+
+			//forward backward
+			if( rFront.distance < rMiddle.distance)
 			{
-				float temp = (rBack.distance - rFront.distance);
-				vAngle.x -= temp* fGravity;
+				Debug.Log ("Adding");
+				vAngle.x -=(rMiddle.distance - rFront.distance);
 			}
 
-			if(rBack.distance < rFront.distance)
+			if( rFront.distance > rMiddle.distance)
 			{
-				float temp = (rFront.distance - rBack.distance);
-				vAngle.x += temp* fGravity;
+				Debug.Log ("Subbing");
+				vAngle.x += (rFront.distance - rMiddle.distance);
 			}
 
-			if(rLeft.distance < rRight.distance)
-			{
-				float temp = (rRight.distance - rLeft.distance);
-				vAngle.z -= temp* fGravity;
-			}
-			if(rRight.distance < rLeft.distance)
-			{
-				float temp = (rLeft.distance - rRight.distance);
-				vAngle.z += temp* fGravity;
-			}
+			//Left and right
+			Quaternion setrotation = new Quaternion();
+			setrotation = Quaternion.LookRotation (transform.forward, rMiddle.normal);
 
 			//Makes sure the object doesn't get angled too far
 			if(vAngle.x < 340 && vAngle.x > 300)
@@ -103,20 +73,13 @@ public class Gravity : MonoBehaviour {
 				vAngle.x = 30;
 				Debug.Log (vAngle.x);
 			}
-			if(vAngle.x < 345 && vAngle.x > 300)
-			{
-				Debug.Log ("x+"+vAngle.x);
-				vAngle.x = 345;				
-			}
-			
-			else if (vAngle.x > 25 && vAngle.x < 90 )
-			{
-				vAngle.x = 25;
-				Debug.Log (vAngle.x);
-			}
+
 
 			transform.eulerAngles = vAngle;
+			transform.rotation = Quaternion.RotateTowards( transform.rotation, setrotation, Time.deltaTime * 20f);
+
 			#endregion
+
 		}
 
 	}
@@ -175,4 +138,24 @@ public class Gravity : MonoBehaviour {
 			//Debug.Log ("x+"+vAngle.x);
 
 				*/
+/*(Vector3.Cross (rRight.point-Vector3.up, rMiddle.point-Vector3.up  )+ Vector3.Cross( rMiddle.point-Vector3.up  , rFront.point-Vector3.up)
+			               + Vector3.Cross (rFront.point-Vector3.up  , rLeft.point-Vector3.up  )+ Vector3.Cross( rLeft.point-Vector3.up , rRight.point-Vector3.up  )).normalized;
+			Debug.Log ("Up Direction"+UpDirection);
+
+		Physics.Raycast (TempPosition - (transform.right * 2)+ (transform.forward *  2), Vector3.down,out rLeft, 500, lWater);
+		Physics.Raycast (TempPosition + (transform.right * 2)+ (transform.forward *  2), Vector3.down,out rRight,500, lWater);
+		//Physics.Raycast (TempPosition - (transform.right * 2), Vector3.down,out rFront, 500, lWater);
+		//Physics.Raycast (TempPosition + (transform.right * 2), Vector3.down,out rMiddle,500, lWater);
+		//Physics.Raycast (TempPosition - (transform.forward *  3), Vector3.down,out rBack, 500,lWater);
+			#region Drawing Raycasta
+		//Debug Drawing the raycasts
+		/*
+		Debug.DrawRay (TempPosition + (transform.forward *  3), Vector3.down, Color.blue);
+		Debug.DrawRay (TempPosition , Vector3.down, Color.blue);
+		//Debug.DrawRay (TempPosition - (transform.forward *  3), Vector3.down, Color.blue);
+		Debug.DrawRay (TempPosition - (transform.right * 2)+ (transform.forward *  2), Vector3.down, Color.green);
+		Debug.DrawRay (TempPosition + (transform.right * 2)+ (transform.forward *  2), Vector3.down, Color.green);
+#endregion
+*/
+	
 #endregion
